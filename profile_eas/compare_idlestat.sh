ac8b867c2d5eb87a5792236273f7ba2b24f7a6a7
Leo Yan <leo.yan@linaro.org>
2015-08-27 23:43:53 +0800

    Add python script to calculate idlestat difference
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- profile_eas/compare_idlestat.sh
+++ profile_eas/compare_idlestat.sh
@@ -18,7 +18,7 @@ compare_trace_file()
 
 	./idlestat --import -f $BASELN/$1/trace.log -b $BASELN/$1/trace.log -r comparison >> $RESULT/$1/idlestat_compare.txt
 
-	echo "== EAS DISE"     >> $RESULT/$1/idlestat_compare.txt
+	echo "== EAS DIS"      >> $RESULT/$1/idlestat_compare.txt
 	echo ""                >> $RESULT/$1/idlestat_compare.txt
 
 	./idlestat --import -f $EASDIS/$1/trace.log -b $BASELN/$1/trace.log -r comparison >> $RESULT/$1/idlestat_compare.txt
@@ -34,6 +34,11 @@ compare_trace_file()
 	./idlestat --import -f $EASHED/$1/trace.log -b $BASELN/$1/trace.log -r comparison >> $RESULT/$1/idlestat_compare.txt
 }
 
+calculate_idle_diff()
+{
+	./calc_idle_diff.py --ifile=$RESULT/$1/idlestat_compare.txt --ofile=$RESULT/$1/idlestat_diff.txt
+}
+
 compare_trace_file mp3
 compare_trace_file rt-app-6
 compare_trace_file rt-app-13
@@ -43,3 +48,13 @@ compare_trace_file rt-app-31
 compare_trace_file rt-app-38
 compare_trace_file rt-app-44
 compare_trace_file rt-app-50
+
+calculate_idle_diff mp3
+calculate_idle_diff rt-app-6
+calculate_idle_diff rt-app-13
+calculate_idle_diff rt-app-19
+calculate_idle_diff rt-app-25
+calculate_idle_diff rt-app-31
+calculate_idle_diff rt-app-38
+calculate_idle_diff rt-app-44
+calculate_idle_diff rt-app-50
