5a1ded027dc968e19c44e4bc6278fcbc87de47f2
Leo Yan <leo.yan@linaro.org>
2015-08-31 00:04:16 +0800

    Calculate total time for cpu run
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- profile_eas/calc_pstate_time.py
+++ profile_eas/calc_pstate_time.py
@@ -10,17 +10,17 @@ import re
 
 class calc_pstate_time(object):
 
-    baseln_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
-    baseln_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
+    baseln_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
+    baseln_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
 
-    easdis_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
-    easdis_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
+    easdis_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
+    easdis_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
 
-    easndm_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
-    easndm_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
+    easndm_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
+    easndm_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
 
-    easchd_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
-    easchd_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0 };
+    easchd_cls = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
+    easchd_cpu = { '208.00MHz':0, '432.00MHz':0, '729.00MHz':0, '960.00MHz':0, '1.20GHz':0, 'cycles':0 };
 
     def __init__(self, ofile):
         try:
@@ -102,12 +102,33 @@ class calc_pstate_time(object):
                             stat1[array[0]] = stat1[array[0]] + time
                             opp = next(infile)
 
+        stat0['cycles'] = stat0['208.00MHz'] * 208 + stat0['432.00MHz'] * 432 + stat0['729.00MHz'] * 729 + stat0['960.00MHz'] * 960 + stat0['1.20GHz'] * 1200
+        stat1['cycles'] = stat1['208.00MHz'] * 208 + stat1['432.00MHz'] * 432 + stat1['729.00MHz'] * 729 + stat1['960.00MHz'] * 960 + stat1['1.20GHz'] * 1200
+
     def calc_percentage(self, a, b):
         percentage = float(a) - float(b)
         if b == 0:
             b = 1.0
         percentage = (1.0 * percentage / float(b)) * 100
-        return float(percentage)
+
+        if percentage > 0:
+            self.fp.write(' %+11.2f' % percentage + "%")
+        else:
+            self.fp.write(' %11.2f' % percentage + "%")
+
+    def add_log(self, stat0, stat1):
+        self.calc_percentage(stat0['208.00MHz'], stat1['208.00MHz'])
+        self.fp.write("|")
+        self.calc_percentage(stat0['432.00MHz'], stat1['432.00MHz'])
+        self.fp.write("|")
+        self.calc_percentage(stat0['729.00MHz'], stat1['729.00MHz'])
+        self.fp.write("|")
+        self.calc_percentage(stat0['960.00MHz'], stat1['960.00MHz'])
+        self.fp.write("|")
+        self.calc_percentage(stat0['1.20GHz'],   stat1['1.20GHz'])
+        self.fp.write("|")
+        self.calc_percentage(stat0['cycles'],    stat1['cycles'])
+        self.fp.write("|\n")
 
     def parse(self, ifile1, ifile2, ifile3, ifile4):
 
@@ -117,175 +138,67 @@ class calc_pstate_time(object):
         self.statistic(ifile4, self.easchd_cls, self.easchd_cpu)
 
         self.fp.write("P-State Statistics\n\n")
-        self.fp.write("-------------------------------------------------------------------------\n")
-        self.fp.write("| Cluster Level (ms)                                                    |\n")
-        self.fp.write("------ ------------------------------------------------------------------\n")
-        self.fp.write("| Item      |   208MHz  |   432MHz  |   729MHz  |   960MHz  |   1.2GHz  |\n");
-        self.fp.write("-------------------------------------------------------------------------\n")
+        self.fp.write("-------------------------------------------------------------------------------------------------\n")
+        self.fp.write("| Cluster Level (ms)                                                                            |\n")
+        self.fp.write("------ ------------------------------------------------------------------------------------------\n")
+        self.fp.write("| Item      |     208MHz  |     432MHz  |     729MHz  |     960MHz  |     1.2GHz  |   Cycles    |\n");
+        self.fp.write("-------------------------------------------------------------------------------------------------\n")
 
         self.fp.write("| MAINLINE  |")
-        self.fp.write(' %9.2f' % self.baseln_cls['208.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cls['208.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cls['432.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cls['432.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cls['729.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cls['729.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cls['960.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cls['960.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cls['1.20GHz'])
+        self.fp.write(' %11.2f' % self.baseln_cls['1.20GHz'])
+        self.fp.write(" |")
+        self.fp.write(' %11.2f' % self.baseln_cls['cycles'])
         self.fp.write(" |\n")
 
         self.fp.write("| EAS DIS   |")
-        percentage = self.calc_percentage(self.easdis_cls['208.00MHz'], self.baseln_cls['208.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cls['432.00MHz'], self.baseln_cls['432.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cls['729.00MHz'], self.baseln_cls['729.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cls['960.00MHz'], self.baseln_cls['960.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cls['1.20GHz'], self.baseln_cls['1.20GHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|\n")
-
+        self.add_log(self.easdis_cls, self.baseln_cls)
 
         self.fp.write("| EAS NDM   |")
-        percentage = self.calc_percentage(self.easndm_cls['208.00MHz'], self.baseln_cls['208.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cls['432.00MHz'], self.baseln_cls['432.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cls['729.00MHz'], self.baseln_cls['729.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cls['960.00MHz'], self.baseln_cls['960.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cls['1.20GHz'], self.baseln_cls['1.20GHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|\n")
-
+        self.add_log(self.easndm_cls, self.baseln_cls)
 
         self.fp.write("| EAS SCHED |")
-        percentage = self.calc_percentage(self.easchd_cls['208.00MHz'], self.baseln_cls['208.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
+        self.add_log(self.easchd_cls, self.baseln_cls)
 
-        percentage = self.calc_percentage(self.easchd_cls['432.00MHz'], self.baseln_cls['432.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
+        self.fp.write("-------------------------------------------------------------------------------------------------\n\n")
 
-        percentage = self.calc_percentage(self.easchd_cls['729.00MHz'], self.baseln_cls['729.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easchd_cls['960.00MHz'], self.baseln_cls['960.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easchd_cls['1.20GHz'], self.baseln_cls['1.20GHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|\n")
-
-        self.fp.write("-------------------------------------------------------------------------\n\n")
-
-        self.fp.write("-------------------------------------------------------------------------\n")
-        self.fp.write("| CPU Level (ms)                                                        |\n")
-        self.fp.write("------ ------------------------------------------------------------------\n")
-        self.fp.write("| Item      |   208MHz  |   432MHz  |   729MHz  |   960MHz  |   1.2GHz  |\n");
-        self.fp.write("-------------------------------------------------------------------------\n")
+        self.fp.write("-------------------------------------------------------------------------------------------------\n")
+        self.fp.write("| CPU Level (ms)                                                                                |\n")
+        self.fp.write("------ ------------------------------------------------------------------------------------------\n")
+        self.fp.write("| Item      |     208MHz  |     432MHz  |     729MHz  |     960MHz  |     1.2GHz  |   Cycles    |\n");
+        self.fp.write("-------------------------------------------------------------------------------------------------\n")
 
         self.fp.write("| MAINLINE  |")
-        self.fp.write(' %9.2f' % self.baseln_cpu['208.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cpu['208.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cpu['432.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cpu['432.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cpu['729.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cpu['729.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cpu['960.00MHz'])
+        self.fp.write(' %11.2f' % self.baseln_cpu['960.00MHz'])
         self.fp.write(" |")
-        self.fp.write(' %9.2f' % self.baseln_cpu['1.20GHz'])
+        self.fp.write(' %11.2f' % self.baseln_cpu['1.20GHz'])
+        self.fp.write(" |")
+        self.fp.write(' %11.2f' % self.baseln_cpu['cycles'])
         self.fp.write(" |\n")
 
         self.fp.write("| EAS DIS   |")
-        percentage = self.calc_percentage(self.easdis_cpu['208.00MHz'], self.baseln_cpu['208.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cpu['432.00MHz'], self.baseln_cpu['432.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cpu['729.00MHz'], self.baseln_cpu['729.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cpu['960.00MHz'], self.baseln_cpu['960.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easdis_cpu['1.20GHz'], self.baseln_cpu['1.20GHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|\n")
-
+        self.add_log(self.easdis_cpu, self.baseln_cpu)
 
         self.fp.write("| EAS NDM   |")
-        percentage = self.calc_percentage(self.easndm_cpu['208.00MHz'], self.baseln_cpu['208.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cpu['432.00MHz'], self.baseln_cpu['432.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cpu['729.00MHz'], self.baseln_cpu['729.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cpu['960.00MHz'], self.baseln_cpu['960.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easndm_cpu['1.20GHz'], self.baseln_cpu['1.20GHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|\n")
-
+        self.add_log(self.easndm_cpu, self.baseln_cpu)
 
         self.fp.write("| EAS SCHED |")
-        percentage = self.calc_percentage(self.easchd_cpu['208.00MHz'], self.baseln_cpu['208.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
+        self.add_log(self.easchd_cpu, self.baseln_cpu)
 
-        percentage = self.calc_percentage(self.easchd_cpu['432.00MHz'], self.baseln_cpu['432.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easchd_cpu['729.00MHz'], self.baseln_cpu['729.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easchd_cpu['960.00MHz'], self.baseln_cpu['960.00MHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|")
-
-        percentage = self.calc_percentage(self.easchd_cpu['1.20GHz'], self.baseln_cpu['1.20GHz'])
-        self.fp.write(' %9.2f' % percentage + "%")
-        self.fp.write("|\n")
-
-        self.fp.write("-------------------------------------------------------------------------\n\n")
+        self.fp.write("-------------------------------------------------------------------------------------------------\n\n")
 
         #print(self.baseln_cls['208.00MHz']);
         #print(self.baseln_cls['432.00MHz']);
