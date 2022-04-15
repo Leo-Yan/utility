8d25e99ab39ae2d16f7985face32f384a5608b29
Leo Yan <leo.yan@linaro.org>
2022-04-15 19:48:34 +0800

    Support build kernel for DB410c
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- kernel/cc_kernel.sh
+++ kernel/cc_kernel.sh
@@ -6,7 +6,7 @@ OUT="${KERNEL}/${PLAT}_out"
 
 if [[ $# -ne 2 ]] ; then
 	echo "Usage: cc_kernel.sh kernel_path platform"
-	echo "       Supoorted platform: hikey960"
+	echo "       Supoorted platform: hikey960, db410c"
 	exit 1
 fi
 
@@ -18,7 +18,11 @@ fi
 function build_kernel_images {
 	pushd $KERNEL
 
-	make -j `nproc` $TARGET $DTB O=$OUT
+	make -j `nproc` $TARGET $DTB modules O=$OUT
+
+	rm -rf $OUT/modules
+	make -j `nproc` O=$OUT INSTALL_MOD_PATH=$OUT/overlay INSTALL_MOD_STRIP=1 modules_install
+	tar --owner root:0 --group root:0 -C $OUT/overlay -cf $OUT/overlay.tgz .
 
 	IMAGE_FILE=$OUT/arch/$ARCH/boot/$TARGET
 	DTB_FILE=$OUT/arch/$ARCH/boot/dts/$DTB
@@ -37,9 +41,33 @@ function build_kernel_images {
 }
 
 function build_abootimg {
-	if [ ! -z ${ABOOTIMG+x} ]; then
+	case "$PLAT" in
+	"hikey960")
 		cat $IMAGE_FILE $DTB_FILE > $OUT/Image-dtb
 		abootimg --create $OUT/boot.img -k $OUT/Image-dtb -r $RAMDISK -f $BOOTIMG_CONF
+		;;
+
+	"db410c")
+		cat $IMAGE_FILE $DTB_FILE > $OUT/Image.gz-dtb
+		echo "not a ramdisk" > ramdisk.img
+		abootimg --create $OUT/boot.img -k $OUT/Image.gz-dtb -r ramdisk.img \
+			-c pagesize=2048 -c kerneladdr=0x80008000 -c ramdiskaddr=0x81000000 \
+			-c cmdline="root=/dev/mmcblk0p14 rw rootwait console=tty0 console=ttyMSM0,115200n8 fastboot crashkernel=256M crash_kexec_post_notifiers nohlt"
+		;;
+
+	* )
+		echo "can't build for $PLAT, wrong platform name?"
+		exit
+		;;
+	esac
+}
+
+function copy_images {
+	if [ ! -z ${COPYIMAGE+x} ]; then
+		cp_to_dbg_machine $IMAGE_FILE
+		cp_to_dbg_machine $DTB_FILE
+		cp_to_dbg_machine $OUT/boot.img
+		cp_to_dbg_machine $OUT/overlay.tgz
 	fi
 }
 
@@ -118,6 +146,77 @@ case "$PLAT" in
 	popd
 
 	;;
+
+"db410c")
+	export ARCH=arm64
+	export CROSS_COMPILE=aarch64-linux-gnu-
+	export TARGET=Image.gz
+	export DTB=qcom/apq8016-sbc.dtb
+	export KERNEL_CONFIG=defconfig
+	export ABOOTIMG=boot.img
+	export COPYIMAGE=1
+
+	pushd $KERNEL
+	make $KERNEL_CONFIG O=$OUT
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_SYSMON
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_PIL_INFO
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_Q6V5_COMMON
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_Q6V5_MSS
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_Q6V5_PAS
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_WCNSS_PIL
+	./scripts/config --file $OUT/.config -e CONFIG_RPMSG_QCOM_GLINK_SMEM
+	./scripts/config --file $OUT/.config -e CONFIG_QCOM_RPROC_COMMON
+	./scripts/config --file $OUT/.config -e CONFIG_WL18XX
+	./scripts/config --file $OUT/.config -e CONFIG_WLCORE_SDIO
+	./scripts/config --file $OUT/.config -e CONFIG_CFG80211
+	./scripts/config --file $OUT/.config -e CONFIG_MAC80211
+	./scripts/config --file $OUT/.config -e CONFIG_MAC80211_MESH
+	./scripts/config --file $OUT/.config -e CONFIG_MAC80211_LEDS
+	./scripts/config --file $OUT/.config -e CONFIG_RFKILL
+	./scripts/config --file $OUT/.config -e CONFIG_RFKILL_GPIO
+	./scripts/config --file $OUT/.config -e CONFIG_WLAN_VENDOR_ADMTEK
+	./scripts/config --file $OUT/.config -e CONFIG_ATH_COMMON
+	./scripts/config --file $OUT/.config -e QCOM_WCNSS_CTRL
+	./scripts/config --file $OUT/.config -e CONFIG_WCN36XX
+	./scripts/config --file $OUT/.config -e CONFIG_USB_RTL8150
+	./scripts/config --file $OUT/.config -e CONFIG_USB_RTL8152
+	./scripts/config --file $OUT/.config -e CONFIG_USB_LAN78XX
+	./scripts/config --file $OUT/.config -e CONFIG_USB_USBNET
+	./scripts/config --file $OUT/.config -e CONFIG_USB_NET_AX8817X
+	./scripts/config --file $OUT/.config -e CONFIG_USB_NET_AX88179_178A
+	./scripts/config --file $OUT/.config -e CONFIG_INTERCONNECT_QCOM_MSM8916
+	./scripts/config --file $OUT/.config -e CONFIG_BPF_SYSCALL
+	./scripts/config --file $OUT/.config -e CONFIG_BPF_JIT_ALWAYS_ON
+	./scripts/config --file $OUT/.config -e CONFIG_TRACEPOINTS
+	./scripts/config --file $OUT/.config -e CONFIG_KPROBES
+	./scripts/config --file $OUT/.config -e CONFIG_UPROBES
+	./scripts/config --file $OUT/.config -e CONFIG_KRETPROBES
+	./scripts/config --file $OUT/.config -e CONFIG_PROC_KCORE
+	./scripts/config --file $OUT/.config -e CONFIG_NOP_TRACER
+	./scripts/config --file $OUT/.config -e CONFIG_TRACER_MAX_TRACE
+	./scripts/config --file $OUT/.config -e CONFIG_RING_BUFFER
+	./scripts/config --file $OUT/.config -e CONFIG_EVENT_TRACING
+	./scripts/config --file $OUT/.config -e CONFIG_CONTEXT_SWITCH_TRACER
+	./scripts/config --file $OUT/.config -e CONFIG_TRACING
+	./scripts/config --file $OUT/.config -e CONFIG_GENERIC_TRACER
+	./scripts/config --file $OUT/.config -e CONFIG_FTRACE
+	./scripts/config --file $OUT/.config -e CONFIG_FUNCTION_TRACER
+	./scripts/config --file $OUT/.config -e CONFIG_FUNCTION_GRAPH_TRACER
+	./scripts/config --file $OUT/.config -e CONFIG_SCHED_TRACER
+	./scripts/config --file $OUT/.config -e CONFIG_FTRACE_SYSCALLS
+	./scripts/config --file $OUT/.config -e CONFIG_TRACER_SNAPSHOT
+	./scripts/config --file $OUT/.config -e CONFIG_KPROBE_EVENTS
+	./scripts/config --file $OUT/.config -e CONFIG_UPROBE_EVENTS
+	./scripts/config --file $OUT/.config -e CONFIG_BPF_EVENTS
+	./scripts/config --file $OUT/.config -e CONFIG_DYNAMIC_EVENTS
+	./scripts/config --file $OUT/.config -e CONFIG_PROBE_EVENTS
+	./scripts/config --file $OUT/.config -e CONFIG_DYNAMIC_FTRACE
+	yes "" | make oldconfig O=$OUT
+	#make allyesconfig O=$OUT
+	popd
+
+	;;
+
 * )
 	echo "can't build for $PLAT, wrong platform name?"
 	exit
@@ -126,3 +225,4 @@ esac
 
 build_kernel_images
 build_abootimg
+copy_images
